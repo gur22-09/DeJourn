73f9622850423733b6bdc451a7c5ae4eb1128097
GURPRIT <tirprug@users.noreply.github.com>
2020-04-01 03:15:45 +0530

    Updates

---
+++ precache-manifest.18dc2b6fcaa8a16eb2e329334224d4b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5bd6b28080260a265004a5ac6af833b",
+    "url": "/DeJourn/index.html"
+  },
+  {
+    "revision": "29e71c23e119c4f3f00a",
+    "url": "/DeJourn/static/css/main.7de788c4.chunk.css"
+  },
+  {
+    "revision": "2f8fbf19dd9347850408",
+    "url": "/DeJourn/static/js/2.9e1e68e3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/DeJourn/static/js/2.9e1e68e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29e71c23e119c4f3f00a",
+    "url": "/DeJourn/static/js/main.498fdad8.chunk.js"
+  },
+  {
+    "revision": "41dabc63d2eddbb8b493",
+    "url": "/DeJourn/static/js/runtime-main.caeaa000.js"
+  }
+]);
